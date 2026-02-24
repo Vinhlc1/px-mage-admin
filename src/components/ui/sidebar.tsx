be06583a1508cf6e0ecd66e1f81a727376cfff3b
Vinh <congvinhlvv@gmail.com>
2026-02-24 08:28:47 +0700

    minor fix

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -606,10 +606,9 @@ function SidebarMenuSkeleton({
 }: React.ComponentProps<"div"> & {
   showIcon?: boolean
 }) {
-  // Random width between 50 to 90%.
-  const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
-  }, [])
+  const id = React.useId()
+  const widths = ['50%', '60%', '70%', '80%', '90%', '55%', '65%', '75%', '85%']
+  const width = widths[id.codePointAt(1)! % widths.length]
 
   return (
     <div
